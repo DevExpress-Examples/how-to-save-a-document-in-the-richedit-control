443d7676901b801ede348b95d45b868d01424764
DevExpressExampleBot <fakeEmail>
2023-04-13 18:51:05 +0400

    Source auto update [skip ci]

--- VB/SaveDocument/CustomCommand.vb
+++ VB/SaveDocument/CustomCommand.vb
@@ -1,47 +1,46 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Text
 Imports DevExpress.XtraRichEdit.Services
 Imports DevExpress.XtraRichEdit
 Imports DevExpress.XtraRichEdit.Commands
 Imports System.Windows.Forms
 
 Namespace SaveDocumentSample
-	#Region "#savecommand"
-	Public Class CustomRichEditCommandFactoryService
-		Implements IRichEditCommandFactoryService
-		Private ReadOnly service As IRichEditCommandFactoryService
-		Private ReadOnly control As RichEditControl
-		Public Sub New(ByVal control As RichEditControl, ByVal service As IRichEditCommandFactoryService)
-			DevExpress.Utils.Guard.ArgumentNotNull(control, "control")
-			DevExpress.Utils.Guard.ArgumentNotNull(service, "service")
-			Me.control = control
-			Me.service = service
-		End Sub
-		Public Function CreateCommand(ByVal id As RichEditCommandId) As RichEditCommand Implements IRichEditCommandFactoryService.CreateCommand
-			If id = RichEditCommandId.FileSaveAs Then
-				Return New CustomSaveDocumentAsCommand(control)
-			End If
 
-			Return service.CreateCommand(id)
-		End Function
-	End Class
+'#Region "#savecommand"
+    Public Class CustomRichEditCommandFactoryService
+        Implements IRichEditCommandFactoryService
 
-	Public Class CustomSaveDocumentAsCommand
-		Inherits SaveDocumentAsCommand
-		Public Sub New(ByVal control As IRichEditControl)
-			MyBase.New(control)
-		End Sub
+        Private ReadOnly service As IRichEditCommandFactoryService
 
-		Protected Overrides Sub ExecuteCore()
-			Dim dialog As SaveFileDialog = New SaveFileDialog With {.Filter = "Rich Text Format Files (*.rtf)|*.rtf|All Files (*.*)|*.*", .FileName = "SavedDocument.rtf", .RestoreDirectory = True, .CheckFileExists = False, .CheckPathExists = True, .OverwritePrompt = True, .DereferenceLinks = True, .ValidateNames = True, .AddExtension = False, .FilterIndex = 1}
-			dialog.InitialDirectory = "C:\Temp"
-			If dialog.ShowDialog() = DialogResult.OK Then
-				CType(Me.Control, RichEditControl).SaveDocument(dialog.FileName, DocumentFormat.Rtf)
-			End If
-			'base.ExecuteCore();
-		End Sub
-	End Class
-		#End Region ' #savecommand
+        Private ReadOnly control As RichEditControl
+
+        Public Sub New(ByVal control As RichEditControl, ByVal service As IRichEditCommandFactoryService)
+            DevExpress.Utils.Guard.ArgumentNotNull(control, "control")
+            DevExpress.Utils.Guard.ArgumentNotNull(service, "service")
+            Me.control = control
+            Me.service = service
+        End Sub
+
+        Public Function CreateCommand(ByVal id As RichEditCommandId) As RichEditCommand Implements IRichEditCommandFactoryService.CreateCommand
+            If id = RichEditCommandId.FileSaveAs Then Return New CustomSaveDocumentAsCommand(control)
+            Return service.CreateCommand(id)
+        End Function
+    End Class
+
+    Public Class CustomSaveDocumentAsCommand
+        Inherits SaveDocumentAsCommand
+
+        Public Sub New(ByVal control As IRichEditControl)
+            MyBase.New(control)
+        End Sub
+
+        Protected Overrides Sub ExecuteCore()
+            Dim dialog As SaveFileDialog = New SaveFileDialog With {.Filter = "Rich Text Format Files (*.rtf)|*.rtf|All Files (*.*)|*.*", .FileName = "SavedDocument.rtf", .RestoreDirectory = True, .CheckFileExists = False, .CheckPathExists = True, .OverwritePrompt = True, .DereferenceLinks = True, .ValidateNames = True, .AddExtension = False, .FilterIndex = 1}
+            dialog.InitialDirectory = "C:\Temp"
+            If dialog.ShowDialog() = DialogResult.OK Then
+                CType(Control, RichEditControl).SaveDocument(dialog.FileName, DocumentFormat.Rtf)
+            End If
+        'base.ExecuteCore();
+        End Sub
+    End Class
+'#End Region  ' #savecommand
 End Namespace
--- VB/SaveDocument/Form1.vb
+++ VB/SaveDocument/Form1.vb
@@ -1,56 +1,53 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
-#Region "#usings"
+'#Region "#usings"
 Imports DevExpress.XtraRichEdit
 Imports DevExpress.XtraRichEdit.Export
 Imports DevExpress.XtraRichEdit.Services
-#End Region ' #usings
 
+'#End Region  ' #usings
 Namespace SaveDocumentSample
-	Partial Public Class Form1
-		Inherits Form
-#Region "#savecommandaddservice"
-		Public Sub New()
-			InitializeComponent()
 
-			Dim commandFactory As New CustomRichEditCommandFactoryService(richEditControl1, richEditControl1.GetService(Of IRichEditCommandFactoryService)())
-			richEditControl1.ReplaceService(Of IRichEditCommandFactoryService)(commandFactory)
-		End Sub
-		#End Region ' #savecommandaddservice
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			richEditControl1.LoadDocument("TextWithImages.rtf", DocumentFormat.Rtf)
-		End Sub
-#Region "#save"
-Private Sub btnSaveToFile_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnSaveToFile.Click
-	richEditControl1.SaveDocument("SavedDocument.rtf", DocumentFormat.Rtf)
-	Dim fi As New System.IO.FileInfo("SavedDocument.rtf")
-	Dim msg As String = String.Format("The size of the file is {0:#,#} bytes.", fi.Length.ToString("#,#"))
-	MessageBox.Show(msg)
-End Sub
+'#Region "#savecommandaddservice"
+        Public Sub New()
+            InitializeComponent()
+            Dim commandFactory As CustomRichEditCommandFactoryService = New CustomRichEditCommandFactoryService(richEditControl1, richEditControl1.GetService(Of IRichEditCommandFactoryService)())
+            richEditControl1.ReplaceService(Of IRichEditCommandFactoryService)(commandFactory)
+        End Sub
 
-Private Sub richEditControl1_BeforeExport(ByVal sender As Object, ByVal e As DevExpress.XtraRichEdit.BeforeExportEventArgs) Handles richEditControl1.BeforeExport
-	Dim checkDocument As DocumentExportCapabilities = richEditControl1.Document.RequiredExportCapabilities
-	If (e.DocumentFormat = DocumentFormat.Rtf) AndAlso checkDocument.InlinePictures Then
-		Dim reduceFileSize As DialogResult = MessageBox.Show("This document contains inline pictures." & Constants.vbLf & "You can embed the same picture in two different types (original and Windows Metafile) for better compatibility" & " although it increases the file size. By default a picture is saved in original format only." & Constants.vbLf & "Enable dual picture format in a saved file?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+'#End Region  ' #savecommandaddservice
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            richEditControl1.LoadDocument("TextWithImages.rtf", DocumentFormat.Rtf)
+        End Sub
 
-		Dim options As RtfDocumentExporterOptions = TryCast(e.Options, RtfDocumentExporterOptions)
-		If options IsNot Nothing Then
-			Select Case reduceFileSize
-			Case System.Windows.Forms.DialogResult.Yes
-					options.Compatibility.DuplicateObjectAsMetafile = True
-			Case System.Windows.Forms.DialogResult.No
-					options.Compatibility.DuplicateObjectAsMetafile = False
-			End Select
-		End If
-	End If
-End Sub
-#End Region '  #save
-	End Class
+'#Region "#save"
+        Private Sub btnSaveToFile_Click(ByVal sender As Object, ByVal e As EventArgs)
+            richEditControl1.SaveDocument("SavedDocument.rtf", DocumentFormat.Rtf)
+            Dim fi As IO.FileInfo = New IO.FileInfo("SavedDocument.rtf")
+            Dim msg As String = String.Format("The size of the file is {0:#,#} bytes.", fi.Length.ToString("#,#"))
+            MessageBox.Show(msg)
+        End Sub
+
+        Private Sub richEditControl1_BeforeExport(ByVal sender As Object, ByVal e As BeforeExportEventArgs)
+            Dim checkDocument As DocumentExportCapabilities = richEditControl1.Document.RequiredExportCapabilities
+            If e.DocumentFormat = DocumentFormat.Rtf AndAlso checkDocument.InlinePictures Then
+                Dim reduceFileSize As DialogResult = MessageBox.Show("This document contains inline pictures." & Microsoft.VisualBasic.Constants.vbLf & "You can embed the same picture in two different types (original and Windows Metafile) for better compatibility" & " although it increases the file size. By default a picture is saved in original format only." & Microsoft.VisualBasic.Constants.vbLf & "Enable dual picture format in a saved file?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+                Dim options As RtfDocumentExporterOptions = TryCast(e.Options, RtfDocumentExporterOptions)
+                If options IsNot Nothing Then
+                    Select Case reduceFileSize
+                        Case DialogResult.Yes
+                            options.Compatibility.DuplicateObjectAsMetafile = True
+                        Case DialogResult.No
+                            options.Compatibility.DuplicateObjectAsMetafile = False
+                    End Select
+                End If
+            End If
+        End Sub
+'#End Region  ' #save
+    End Class
 End Namespace
